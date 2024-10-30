7c332ae3b069bc1e116d2a36d7d462ab61d6b8cf
Simon Shine <simon@simonshine.dk>
2024-10-30 06:08:17 +0100

    fix(default.nix): Add buildPhase, installPhase, checkPhase
    Also replace 'rustup' with 'rustc' and 'cargo'
    
    Also, split nativeBuildInputs so openssl is in buildInputs.

--- default.nix
+++ default.nix
@@ -1,9 +1,33 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.stdenv.mkDerivation {
+  # This derivation produces a Nix package
   name = "hello-world";
+  version = "0.1.0";
+  src = ./.;
+
+  # Build dependencies
   nativeBuildInputs = with pkgs; [
-    rustup
+    rustc
+    cargo
     pkg-config
+  ];
+
+  # Runtime dependencies
+  buildInputs = with pkgs; [
     openssl
   ];
+
+  buildPhase = ''
+    cargo build --release
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp target/release/hello-world $out/bin/
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    cargo test
+  '';
 }
